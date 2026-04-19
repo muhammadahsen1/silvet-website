da21af86439ed6eb2ab53d57f7a000b33c3db383
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2026-04-19 07:35:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function shopNow() {
-    alert("Welcome to Silvet, Qasim Abbas!");
+let cart = [];
+
+function addToCart(item) {
+  cart.push(item);
+  updateCart();
+}
+
+function updateCart() {
+  document.getElementById("cartCount").innerText = cart.length;
+
+  let list = document.getElementById("cartItems");
+  list.innerHTML = "";
+
+  cart.forEach((item, index) => {
+    let li = document.createElement("li");
+    li.innerHTML = item + " <button onclick='removeItem(" + index + ")'>X</button>";
+    list.appendChild(li);
+  });
+}
+
+function removeItem(index) {
+  cart.splice(index, 1);
+  updateCart();
+}
+
+function clearCart() {
+  cart = [];
+  updateCart();
+}
+
+function toggleCart() {
+  let box = document.getElementById("cartBox");
+  box.style.display = box.style.display === "block" ? "none" : "block";
 }
